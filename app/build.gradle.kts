fc022fb44516f95ed5836ecd1f7271d6b21a58bf
mrenann <marcos.renann.br@gmail.com>
2023-12-02 18:54:59 -0300

    chore: dependecies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(libs.voyager.androidx)
+    implementation(libs.voyager.navigator)
+    
+    //Testes
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
