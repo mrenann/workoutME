7947fabd05c00eee5a0f69b42abf5d16f0fb3e02
mrenann <marcos.renann.br@gmail.com>
2023-12-04 02:56:10 -0300

    chore: move exercises Tab

--- app/src/main/java/br/mrenann/workoutme/view/steps/home/tabs/exercisesTab/ExercisesTab.kt
+++ app/src/main/java/br/mrenann/workoutme/view/steps/home/tabs/exercisesTab/ExercisesTab.kt
@@ -1,29 +1,22 @@
-package br.mrenann.workoutme.view.steps.home.tabs
+package br.mrenann.workoutme.view.steps.home.tabs.exercisesTab
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Call
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
-import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
-import compose.icons.FeatherIcons
-import compose.icons.feathericons.Eye
-import compose.icons.feathericons.Home
+import compose.icons.EvaIcons
+import compose.icons.evaicons.Outline
+import compose.icons.evaicons.outline.Compass
 
-object HomeTab3 : Tab {
+object exercisesTab : Tab {
 
     override val options: TabOptions
         @Composable
         get() {
-            val title = "THREE"
-            val icon = rememberVectorPainter(Icons.Default.Call)
+            val title = "Exercicios"
+            val icon = rememberVectorPainter(EvaIcons.Outline.Compass)
             return remember {
                 TabOptions(
                     index = 2u,
